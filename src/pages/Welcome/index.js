997eb53345a0025dec773137df27c23fe1aef03f
Netson Cavina <netsoncavina@gmail.com>
2023-03-02 20:05:31 -0300

    Criação da pagina Welcome

---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,11 @@
+rn;
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Welcome() {
+  return (
+    <View>
+      <Text>Tela Welcome</Text>
+    </View>
+  );
+}
