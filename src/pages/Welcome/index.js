6b5ac33e0f9789c7f23c3dc20b1dedf98fc3e1d5
Netson Cavina <netsoncavina@gmail.com>
2023-03-02 20:44:48 -0300

    adição das views e estilo

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,10 +1,72 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import React from "react";
 
 export default function Welcome() {
   return (
-    <View>
-      <Text>Tela Welcome</Text>
+    <View style={styles.container}>
+      <View style={styles.containerLogo}>
+        <Image
+          source={require("../../assets/logo.png")}
+          style={{ width: "100%" }}
+          resizeMode="contain"
+        />
+      </View>
+      <View style={styles.containerForm}>
+        <Text style={styles.title}>
+          Encontra mesas e aventuras a qualquer momento!
+        </Text>
+        <Text style={styles.text}>Faça login para começar sua jornada</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#b02b2e",
+  },
+  containerLogo: {
+    flex: 2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  containerForm: {
+    flex: 1,
+    backgroundColor: "#3f0f12",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: "5%",
+    paddingEnd: "5%",
+  },
+  title: {
+    fontSize: 24,
+    color: "#fff",
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 28,
+    marginBottom: 12,
+  },
+  text: {
+    fontSize: 16,
+    color: "#a1a1a1",
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#fff",
+    width: "100%",
+    height: 40,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "#b02b2e",
+    fontWeight: "bold",
+  },
+});
