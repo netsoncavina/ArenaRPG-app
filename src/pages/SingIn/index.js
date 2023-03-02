70bc135445b1fdbd222c74bf87822f7340473879
Netson Cavina <netsoncavina@gmail.com>
2023-03-02 20:05:24 -0300

    Criação da pagina SignIn

---
+++ src/pages/SingIn/index.js
@@ -0,0 +1,11 @@
+rn;
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function SignIn() {
+  return (
+    <View>
+      <Text>Tela Login</Text>
+    </View>
+  );
+}
