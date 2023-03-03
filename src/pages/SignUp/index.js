7e27bd8c44bc95849dc57572460ecabee1532212
Netson Cavina <netsoncavina@gmail.com>
2023-03-03 20:38:29 -0300

    Desenvolvimento da tela de SignUp

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -10,8 +10,101 @@ import * as Animatable from "react-native-animatable";
 
 export default function SingUp() {
   return (
-    <View>
-      <Text>SingUp</Text>
+    <View style={styles.container}>
+      <Animatable.View
+        animation="fadeInLeft"
+        delay={500}
+        style={styles.containerHeader}
+      >
+        <Text style={styles.message}>Realize seu cadastro aventureiro!</Text>
+      </Animatable.View>
+      <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+        <Text style={styles.label}>Nome</Text>
+        <TextInput style={styles.input} placeholder="Digite seu nome..." />
+
+        <Text style={styles.label}>Apelido</Text>
+        <TextInput style={styles.input} placeholder="Digite seu apelido..." />
+
+        <Text style={styles.label}>E-mail</Text>
+        <TextInput style={styles.input} placeholder="Digite seu email..." />
+
+        <Text style={styles.label}>Senha</Text>
+        <TextInput
+          style={styles.password}
+          placeholder="Digite sua senha..."
+          secureTextEntry={true}
+        />
+
+        <Text style={styles.label}>Confirme sua senha</Text>
+        <TextInput
+          style={styles.password}
+          placeholder="Confirme sua senha..."
+          secureTextEntry={true}
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Registrar</Text>
+        </TouchableOpacity>
+      </Animatable.View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#b02b2e",
+  },
+  containerHeader: {
+    marginTop: "14%",
+    marginBottom: "8%",
+    paddingStart: "5%",
+  },
+  message: {
+    fontSize: 28,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  containerForm: {
+    backgroundColor: "#3f0f12",
+    flex: 1,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: "5%",
+    paddingEnd: "5%",
+  },
+  label: {
+    fontSize: 20,
+    marginTop: 28,
+    color: "#fff",
+  },
+  input: {
+    borderBottomWidth: 1,
+    height: 40,
+    marginBottom: 12,
+    fontSize: 16,
+    color: "#fff",
+  },
+  button: {
+    backgroundColor: "#fff",
+    width: "100%",
+    height: 40,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "#b02b2e",
+    fontWeight: "bold",
+  },
+
+  password: {
+    borderBottomWidth: 1,
+    height: 40,
+    marginBottom: 12,
+    fontSize: 16,
+    color: "#fff",
+  },
+});
