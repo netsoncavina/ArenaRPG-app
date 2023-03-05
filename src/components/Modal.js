c6a6b3b9c9fd168971a5d9fd1d2e27e2ba004a0f
Netson Cavina <netsoncavina@gmail.com>
2023-03-05 02:31:29 -0300

    configuração inicial do modal

---
+++ src/components/Modal.js
@@ -0,0 +1,10 @@
+import { View, Text, Modal } from "react-native";
+import React from "react";
+
+export default function Modal(message, visible, setVisible) {
+  return (
+    <View>
+      <Text>Modal</Text>
+    </View>
+  );
+}
