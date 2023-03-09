7a46d2738f366710aa4d8243636310dbae54e691
Netson Cavina <netsoncavina@gmail.com>
2023-03-08 23:14:38 -0300

    adicionado lista de icones

---
+++ src/components/utils/index.js
@@ -0,0 +1,64 @@
+const icons = [
+  {
+    image: require("../../assets/icons/Aharadak.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Allihanna.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Arsenal.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Azgher.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Hynnin.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Kallyadranoch.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Khalmir.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Lena.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Lin-Wu.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Marah.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Megalokk.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Nimb.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Oceano.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Ssszaas.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Tanna-Toh.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Tenebra.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Thwor.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Thyatis.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Valkaria.jpg"),
+  },
+  {
+    image: require("../../assets/icons/Wynna.jpg"),
+  },
+];
+
+export default icons;
