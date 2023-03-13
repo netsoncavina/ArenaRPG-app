71eb5133df4ae04a7a494f8bccb77e7659eecef8
Netson Cavina <netsoncavina@gmail.com>
2023-03-13 00:26:21 -0300

    adicionado conexão com backend

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,12 +1,20 @@
-import React from "react";
-import {
-  StyleSheet,
-  View,
-  ScrollView,
-} from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, ScrollView, Text } from "react-native";
 import Post from "./Post/Post";
 
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://192.168.15.18:5000/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <>
       <ScrollView
@@ -17,48 +25,22 @@ const Posts = () => {
         }}
         style={styles.container}
       >
-        <View style={{ height: 20 }} />
-        <Post
-          image={
-            "https://uploads.jovemnerd.com.br/wp-content/uploads/2022/12/nc862_rpg_ghanor_4__z343lvoq-1210x544.jpg"
-          }
-          title={"Procura-se player"}
-          author={"VoidNoxian"}
-          content={"Procura-se player para uma aventura épica"}
-          system={"Ghanor"}
-          type={"Mesa"}
-        />
-
-        <Post
-          image={
-            "https://uploads.jovemnerd.com.br/wp-content/uploads/2021/11/NC805-magias-do-rpg-1210x544.png"
-          }
-          title={"Guia de magias"}
-          author={"Ignis Crae"}
-          content={"Procura-se player para uma aventura épica"}
-          system={"D&D"}
-          type={"Off Topic"}
-        />
-        <Post
-          image={
-            "https://uploads.jovemnerd.com.br/wp-content/uploads/2021/01/NC_761-bastidores-ncrpg-1210x544.png"
-          }
-          title={"Formação de mesa"}
-          author={"Rexthor   "}
-          content={"Procura-se player para uma aventura épica"}
-          system={"Cthulhu"}
-          type={"Mesa"}
-        />
-        <Post
-          image={
-            "https://uploads.jovemnerd.com.br/wp-content/uploads/RPG_CYBERPUNK_FINAL_opt-1210x544.jpg"
-          }
-          title={"Duvidas sobre Cyberpunk"}
-          author={"Arius   "}
-          content={"Procura-se player para uma aventura épica"}
-          system={"3D&T"}
-          type={"Off Topic"}
-        />
+        {isLoading ? (
+          <Text>Carregando...</Text>
+        ) : (
+          posts.map((post) => {
+            return (
+              <Post
+                image={post.image}
+                title={post.title}
+                author={post.author}
+                content={post.content}
+                system={post.system}
+                type={post.type}
+              />
+            );
+          })
+        )}
       </ScrollView>
     </>
   );
