d12b8bc6510d2ac3befe4f09f08b967a50439f16
Netson Cavina <netsoncavina@gmail.com>
2023-03-24 05:17:14 -0300

    Adicionado sistema de like

--- src/components/Posts/Post/PostView/Comments/Comments.js
+++ src/components/Posts/Post/PostView/Comments/Comments.js
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Image, ActivityIndicator } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ActivityIndicator,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import icons from "../../../../utils/index";
 
-const Comments = ({ author, text }) => {
+const Comments = ({
+  userInfo,
+  commentId,
+  author,
+  text,
+  likes,
+  deslikes,
+  edited,
+  createdAt,
+  answers,
+}) => {
   const [authorInfo, setAuthorInfo] = useState("");
   const [image, setImage] = useState("");
-  const [isIcon, setIsIcon] = useState(false);
+  const [isLiked, setIsLiked] = useState("");
 
   const getAuthorInfo = async () => {
     try {
@@ -20,8 +38,29 @@ const Comments = ({ author, text }) => {
     }
   };
 
+  const handleLike = async () => {
+    try {
+      fetch(`http://192.168.15.18:5000/comments/like/${commentId}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: userInfo._id,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setIsLiked(!isLiked);
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   useEffect(() => {
     getAuthorInfo();
+    likes.includes(userInfo._id) ? setIsLiked(true) : setIsLiked(false);
   }, []);
 
   return (
@@ -45,6 +84,16 @@ const Comments = ({ author, text }) => {
       <View style={styles.commentContent}>
         <Text>{text}</Text>
       </View>
+
+      <View style={styles.bottomRow}>
+        <TouchableOpacity onPress={handleLike}>
+          {isLiked ? (
+            <Ionicons name="heart" size={24} color="red" />
+          ) : (
+            <Ionicons name="heart-outline" size={24} color="black" />
+          )}
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -55,6 +104,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: "100%",
+    flexDirection: "column",
     borderBottomWidth: 0.9,
     paddingTop: 10,
     paddingBottom: 10,
@@ -72,7 +122,15 @@ const styles = StyleSheet.create({
   },
   commentContent: {
     width: "100%",
-    height: "100%",
+    // height: "100%",
     paddingLeft: 32,
+  },
+  bottomRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingLeft: 32,
+    paddingTop: 15,
   },
 });
