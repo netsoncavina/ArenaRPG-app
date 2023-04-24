e1b3cd15bc463b6b2f68595a9d74821f9995194f
Netson Cavina <netsoncavina@gmail.com>
2023-04-23 21:03:56 -0300

    Refatoração DeleteModal

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -7,6 +7,7 @@ import { Ionicons } from "@expo/vector-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faDiceD20 } from "@fortawesome/free-solid-svg-icons";
 import PostView from "./PostView/PostView";
+import DeleteModal from "../../Modals/DeleteModal";
 
 const Post = ({
   title,
@@ -179,37 +180,13 @@ const Post = ({
           </>
         )}
       </View>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={isDeleteModalVisible}
-        onRequestClose={() => {
-          setIsDeleteModalVisible(!isDeleteModalVisible);
-        }}
-      >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Deseja deletar o post?</Text>
-            <View style={styles.modalButtons}>
-              <Pressable
-                style={[styles.button, styles.buttonClose]}
-                onPress={() => setIsDeleteModalVisible(!isDeleteModalVisible)}
-              >
-                <Text style={styles.textStyle}>Cancelar</Text>
-              </Pressable>
-              <Pressable
-                style={[styles.button, styles.buttonClose]}
-                onPress={() => {
-                  setIsDeleteModalVisible(!isDeleteModalVisible);
-                  handleDeletePost();
-                }}
-              >
-                <Text style={styles.textStyle}>Deletar</Text>
-              </Pressable>
-            </View>
-          </View>
-        </View>
-      </Modal>
+      <DeleteModal
+        modalVisible={isDeleteModalVisible}
+        setModalVisible={setIsDeleteModalVisible}
+        handleDelete={handleDeletePost}
+        type={"post"}
+      />
+
       {showPost && (
         <PostView
           title={title}
@@ -295,62 +272,6 @@ const styles = StyleSheet.create({
     width: 350,
   },
   interactionIcon: {
-    margin: 5,
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 10,
-  },
-  modalView: {
-    margin: 10,
-    backgroundColor: "white",
-    borderRadius: 10,
-    padding: 10,
-    alignItems: "center",
-    shadowColor: "#000",
-    width: 300,
-    height: 150,
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.5,
-    elevation: 5,
-  },
-  modalButtons: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: 200,
-  },
-  button: {
-    borderRadius: 10,
-    padding: 10,
-    elevation: 2,
-    margin: 5,
-  },
-  buttonOpen: {
-    backgroundColor: "#F194FF",
-  },
-  buttonClose: {
-    backgroundColor: "#1e1e1e",
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  modalText: {
-    fontSize: 18,
-    marginBottom: 15,
-    textAlign: "center",
-  },
-  icon: {
-    width: 30,
-    height: 30,
-    borderRadius: 15,
     margin: 5,
   },
 });
